97344788a929c2a8945abc7d9f85eadd75a7acec
Fikri Bill Gufran <billgufran@gmail.com>
2023-05-07 16:52:02 +0800

    Initial Commit

---
+++ SpaceNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceNews
+//
+//  Created by Muhammad Fikri Bill Gufran on 7/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
